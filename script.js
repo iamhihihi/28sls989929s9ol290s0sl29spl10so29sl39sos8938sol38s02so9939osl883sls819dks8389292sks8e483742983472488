951ee66665132dff73df1ad6be4a5a21d2dca823
iamhihihi <evanmb75@outlook.com>
2025-04-12 17:03:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function passcheck() {
   var thing = document.getElementById("pBox");
   var otherthing = thing.value;
-  if(otherthing == "hi") {
+  if(otherthing == "xH_d.:QM.|)ii+)$`DLY&7YSzm3W\A+Jj1aT1z!1WsFyD=R?#z") {
   return true;                        
   }
   alert("Access denied!");
