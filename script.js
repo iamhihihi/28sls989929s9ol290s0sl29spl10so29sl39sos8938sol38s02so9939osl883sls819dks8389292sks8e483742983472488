7ba79470693ff8d670e6bce5620b5160e98292d4
iamhihihi <evanmb75@outlook.com>
2025-04-12 12:03:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+function passcheck() {
+  var thing = document.getElementById("pBox");
+  var otherthing = thing.value;
+  if(otherthing == "hi") {
+  return true;                        
+  }
+  alert("Access denied!");
+  return false;
+}
